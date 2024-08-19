5167f04be9358e1bb0f1dce470923ba40d6efde8
Le Quoc Ngo <lengo.vq@gmail.com>
2024-08-19 20:41:23 +0700

    fix code

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
 const axios = require('axios');
+app.use(express.static('static'))
 require('dotenv').config();
 
 //create bot by Telegraf library
 const { Telegraf} = require('telegraf');
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(proces.env.BOT_TOKEN);
 
 //cau hinh webhook
 const webhookPath = '/webhook';
@@ -27,16 +28,22 @@ bot.command('start', ctx => {
     bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to CoolStar Bot', {});
 })
 bot.command('bitcoin', ctx => {
-    var rate;
     console.log(ctx.from);
     axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
     .then(response => {
-        console.log(response.data)
-        rate = response.data.bitcoin
-        const message = `Hello, today the Bitcoin price is ${rate.usd}USD`
-        bot.telegram.sendMessage(ctx.chat.id, message, {
-        })
+        console.log(response.data);
+        if (response.data.bitcoin && response.data.bitcoin.usd) {
+            const rate = response.data.bitcoin;
+            const message = `Hello, today the Bitcoin price is ${rate.usd} USD`;
+            bot.telegram.sendMessage(ctx.chat.id, message);
+        } else {
+            bot.telegram.sendMessage(ctx.chat.id, 'Không thể lấy giá Bitcoin.');
+        }
     })
-})
+    .catch(error => {
+        console.error('Lỗi khi gọi API:', error);
+        bot.telegram.sendMessage(ctx.chat.id, 'Có lỗi xảy ra khi lấy giá Bitcoin.');
+    });
+});
 
 app.listen(port, () => console.log(`Listening on ${port}`))
